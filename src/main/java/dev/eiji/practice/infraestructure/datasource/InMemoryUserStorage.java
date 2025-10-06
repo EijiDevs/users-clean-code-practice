e41eb74abb9aaaaeab1e7c50c4c369173f36ff45
Esteban Perafan <bestebanperafan@gmail.com>
2025-10-06 06:46:25 -0300

    feat(infrastructure:datasource): change 'id' to 'username' in the memory user storage to mantain an ubiquitous language

--- src/main/java/dev/eiji/practice/infraestructure/datasource/InMemoryUserStorage.java
+++ src/main/java/dev/eiji/practice/infraestructure/datasource/InMemoryUserStorage.java
@@ -1,52 +1,1 @@
-package dev.eiji.practice.infraestructure.datasource;
-
-import dev.eiji.practice.domain.users.models.User;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-
-/**
- * Singleton in memory storage to proofs without real infrastructure behind
- * This class emulate an SQL Database
- * @author Esteban Perafan
- * @version 1.0
- * @since 0.1.0
- */
-final public class InMemoryUserStorage {
-    private static InMemoryUserStorage instance;
-    private final Map<String, User> users;
-
-    private InMemoryUserStorage() {
-        this.users = new HashMap<>();
-    }
-
-    public static InMemoryUserStorage create() {
-        if (instance == null) {
-            instance = new InMemoryUserStorage();
-        }
-        return instance;
-    }
-
-    public List<User> selectAll() {
-        return new ArrayList<>(users.values());
-    }
-
-    public User selectById(String id) {
-        return users.get(id);
-    }
-
-    public void insert(User user) {
-        users.put(user.getId(), user);
-    }
-
-    public void update(User user) {
-        users.put(user.getId(), user);
-    }
-
-    public void delete(String id) {
-        users.remove(id);
-    }
-}
+package dev.eiji.practice.infraestructure.datasource;import dev.eiji.practice.domain.users.models.User;import java.util.ArrayList;import java.util.HashMap;import java.util.List;import java.util.Map;/** * Singleton in memory storage to proofs without real infrastructure behind * This class emulate an SQL Database * @author Esteban Perafan * @version 1.0 * @since 0.1.0 */final public class InMemoryUserStorage {    private static InMemoryUserStorage instance;    private final Map<String, User> users;    private InMemoryUserStorage() {        this.users = new HashMap<>();    }    public static InMemoryUserStorage create() {        if (instance == null) {            instance = new InMemoryUserStorage();        }        return instance;    }    public List<User> selectAll() {        return new ArrayList<>(users.values());    }    public User selectByUsername(String id) {        return users.get(id);    }    public void insert(User user) {        users.put(user.getUsername(), user);    }    public void update(User user) {        users.put(user.getUsername(), user);    }    public void delete(String id) {        users.remove(id);    }}
