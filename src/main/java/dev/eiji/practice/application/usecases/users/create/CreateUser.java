e65eed30dccd5288f3db8e94002f040aa2388979
Esteban Perafan <bestebanperafan@gmail.com>
2025-10-05 22:54:41 -0300

    feat(users/application:usecases): add 'CreateUser' use case
    * Manage password encryption \n * Save user in memory storage via repository \n  * Create user with an random UUID from application instead infrastructure

--- src/main/java/dev/eiji/practice/application/usecases/users/create/CreateUser.java
+++ src/main/java/dev/eiji/practice/application/usecases/users/create/CreateUser.java
@@ -1,16 +1,32 @@
 package dev.eiji.practice.application.usecases.users.create;
 
+import dev.eiji.practice.application.ports.out.encryption.Encryptor;
+import dev.eiji.practice.domain.users.models.User;
 import dev.eiji.practice.domain.users.ports.UserRepository;
+
+import java.util.UUID;
 
 public class CreateUser {
 
     private final UserRepository respository;
+    private final Encryptor encryptor;
 
-    public CreateUser(UserRepository respository) {
+    public CreateUser(UserRepository respository, Encryptor encryptor) {
         this.respository = respository;
+        this.encryptor = encryptor;
     }
 
     public void create(CreateUserDTO dto) {
+        User user = User.create(
+                UUID.randomUUID().toString(),
+                dto.username(),
+                dto.name(),
+                dto.lastName(),
+                dto.email(),
+                encryptor.encrypt(dto.password()),
+                dto.birthDay()
+        );
 
+        respository.save(user);
     }
 }
